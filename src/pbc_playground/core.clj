45edf0a7d14dc1a7add805f9f51eb83dd72a498c
Markus Hjort <markus.hjort@gmail.com>
2020-04-24 17:09:17 +0300

    Add code validation

--- src/pbc_playground/core.clj
+++ src/pbc_playground/core.clj
@@ -7,24 +7,35 @@
 (-> (buddy-hash/sha256 s)
     (codecs/bytes->hex)))
 
+(defn- serialise-state [state]
+  (pr-str state))
+
 (defn- generate-transaction [data previous]
   (let [data {:timestamp (OffsetDateTime/now)
+              :validator "(fn [input output] (= output (inc input)))"
               :data data
               :precedingHash (:hash previous)}]
-    {:input previous
-     :output {:hash (sha256 (pr-str data)) :data data}}))
+     {:hash (sha256 (serialise-state data)) :data data}))
 
 (defn- add-transaction [chain transaction]
-  (cons (:output transaction) chain))
+  (cons transaction chain))
 
 (defn- add-new-transaction [chain data]
   (let [previous(first chain)
         transaction (generate-transaction data previous)]
     (add-transaction chain transaction)))
 
-(defn- validate-transaction [input output]
+(defn- validate-transaction-hash [input output]
   (and (= (:hash input) (-> output :data :precedingHash))
-       (= (:hash output) (sha256 (pr-str (:data output))))))
+       (= (:hash output) (sha256 (serialise-state (:data output))))))
+
+(defn- validate-transaction-code [input output]
+  (let [validator-fn (-> output :data :validator read-string eval)]
+    (validator-fn (-> input :data :data) (-> output :data :data))))
+
+(defn- validate-transaction [input output]
+  (and (validate-transaction-hash input output)
+       (validate-transaction-code input output)))
 
 (defn- validate-chain [chain]
   (if (= 1 (count chain))
@@ -33,20 +44,24 @@
       false
       (validate-chain (rest chain)))))
 
-(let [chain (-> (add-new-transaction [] "a")
-    (add-new-transaction "b"))]
+(let [transaction1 (generate-transaction 1 nil)
+      transaction2 (generate-transaction 3 transaction1)]
+  (validate-transaction-code transaction1 transaction2))
+
+(let [chain (-> (add-new-transaction [] 1)
+    (add-new-transaction 2))]
   (validate-chain chain))
 
-(let [chain (-> (add-new-transaction [] "a"))]
+(let [chain (-> (add-new-transaction [] 1))]
   (validate-chain chain))
 
-(let [chain (-> (add-new-transaction [] "a")
-                (add-new-transaction "b")
-                (add-new-transaction "c"))]
+(let [chain (-> (add-new-transaction [] 1)
+                (add-new-transaction 2)
+                (add-new-transaction 4))]
   (validate-chain chain))
 
-(let [real-transaction (generate-transaction "a" nil)
-      fake-transaction (assoc-in (generate-transaction "fake" real-transaction) [:output :hash] "fake-hash")
+(let [real-transaction (generate-transaction 1 nil)
+      fake-transaction (assoc (generate-transaction 2 real-transaction) :hash "fake-hash")
       invalid-chain (-> (add-transaction [] real-transaction)
                         (add-transaction fake-transaction))]
   (validate-chain invalid-chain))
